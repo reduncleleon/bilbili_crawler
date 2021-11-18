ad29d0ea1d5b3a290f09bfb19f6fe4410461a0bd
WeiPing Ge <gewp0929@163.com>
2021-11-18 18:27:19 +0800

    db数据量更新到了5000+

--- analyser.py
+++ analyser.py
@@ -2,13 +2,14 @@ import time
 import numpy as np
 import matplotlib
 import wordcloud
+import pyecharts
 import sqlite3
 from bilibili_api import comment, sync , video, settings,Credential
 import asyncio
 
 async def main():
     conn = sqlite3.connect('data.db')
-    cursor = conn.execute("select bvid from bilibili_rank_video limit 0,10 ")
+    cursor = conn.execute("select bvid from bilibili_rank_video limit 10,1000 ")
     for it in cursor:
         bid = ''.join(it)
         try:
@@ -22,14 +23,13 @@ async def main():
             print(e)
         # 打印视频信息
         print(display)
-        # result = np.array(display)
-        # command = "insert into video_info \
-        #                  values(?,?,?,?,?,?,?,?,?,?);"
-        # try:
-        #     conn.execute(command, result)
-        # except Exception as e:
-        #     print(e)
-        #     conn.rollback()
+        result = np.array(display)
+        command = "insert into video_info \
+                         values(?,?,?,?,?,?,?,?,?,?);"
+        try:
+            conn.execute(command, result)
+        except Exception as e:
+            print(e)
         time.sleep(0.1)
 
     conn.commit()
